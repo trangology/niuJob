9c06a74cfbd2508811dcecc55fd1c4dfd624bd75
tsnanh <tsnanh0812@gmail.com>
2020-05-21 12:36:02 +0700

    gnaot

--- android/app/src/main/kotlin/com/example/niu_job/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.niu_job
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/candicate/create_resume/bloc/last_step_bloc/bloc.dart
+++
@@ -1,3 +0,0 @@
-export 'last_step_bloc.dart';
-export 'last_step_event.dart';
-export 'last_step_state.dart';
